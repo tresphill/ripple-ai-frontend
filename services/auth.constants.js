9e1024365c184fca6c34a2218bca7af639396f4d
tphill <tphill760@gmail.com>
2024-01-10 14:31:20 -0500

    backend deployed

--- services/auth.constants.js
+++ services/auth.constants.js
@@ -1,6 +1,5 @@
-export const API_URL =
-  "https://8000-tresphill-rippleai2-gwyip31yybf.ws-us106.gitpod.io/api/v1/";
+export const API_URL = "https://python-tphill760.replit.app/api/v1/";
 export const LOGIN_ENDPOINT = "login/access-token";
 export const REGISTER_ENDPOINT = "users/register/";
 export const REFRESH_ENDPOINT = "token/refresh/";
-export const GETUSER_ENDPOINT = "users"
+export const GETUSER_ENDPOINT = "users";
